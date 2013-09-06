853e9fca349a5b326fc1b47b00e3ff8ee0c99d84
Boris <just-boris@hotmail.com>
2013-09-07 00:12:28 +0400

    поправил анимацию добавил prefixfree доделал стили

--- folio.js
+++ folio.js
@@ -1,47 +1,23 @@
-$.fn.folio = function(config) {
-    var transitionEndEvent = (function getTransitionEvent(){
-        var el = document.createElement('fakeelement'),
-            transitions = {
-            'transition':'transitionend',
-            'OTransition':'oTransitionEnd',
-            'MozTransition':'transitionend',
-            'WebkitTransition':'webkitTransitionEnd'
-        },
-        t;
-        for(t in transitions){
-            if( el.style[t] !== undefined ){
-                return transitions[t];
-            }
-        }
-    })();
-    function animCallback(elm, callback) {
-        if(!transitionEndEvent) {
-            callback();
-        }
-        else {
-            elm.one(transitionEndEvent, callback)
-        }
-    }
-    function selectSilde(index) {
+$.fn.folio = function() {
+    function selectSlide(index) {
         if(index === currentIndex) {
             return;
         }
-        var nextSlide = slides.eq(index),
-            currentSlide = slides.eq(currentIndex);
-        currentIndex = index;
-        nextSlide.addClass(ACTIVE_CLS);
-        nextSlide.addClass(COMING_CLS);
+        if((index - currentIndex) > 0) {
+            for(;currentIndex !== index;currentIndex++) {
+                slides.eq(currentIndex+1).addClass(ACTIVE_CLS);
+            }
+        }
+        else {
+            for(;currentIndex !== index;currentIndex--) {
+                slides.eq(currentIndex).removeClass(ACTIVE_CLS);
+            }
+        }
         updateSlider();
-        animCallback(nextSlide, function() {
-            currentSlide.removeClass(ACTIVE_CLS);
-            animCallback(currentSlide, function() {
-                nextSlide.removeClass(COMING_CLS);
-            });
-        });
     }
     function changeSlide(direction) {
-        var next = (currentIndex + direction) % slides.length;
-        selectSilde(next);
+        var next = (slides.length + currentIndex + direction) % slides.length;
+        selectSlide(next);
     }
     function next() {
         changeSlide(+1);
@@ -69,35 +45,33 @@ $.fn.folio = function(config) {
         slider.css('left', getSliderPosition(currentIndex)*100+"%");
     }
 
-    var COMING_CLS = 'folio-pane_coming',
-        ACTIVE_CLS = 'folio-pane_active',
+    var ACTIVE_CLS = 'folio-pane_active',
         currentIndex=0,
         slider = this.find('.folio-slider'),
         navbar = this.find('.folio-nav'),
         slides = this.find('.folio-pane');
-    config = config || {};
-    if(config.verticalSlide) {
-        this.addClass('folio-vertical')
-    }
-
 
     slides.on('click', next);
     this.find('.folio-arrow-right').on('click', next);
     this.find('.folio-arrow-left').on('click', previous);
     navbar.on('click', function(event) {
-        selectSilde(getNearestSlide(event.offsetX/navbar.width()));
+        selectSlide(getNearestSlide((event.pageX-navbar.offset().left)/navbar.width()));
     });
     slider.on('mousedown', function() {
         function moveHandler(event) {
-            var pos = Math.min(1, Math.max(0, (event.clientX-navbar.offset().left)/navbar.width()));
-            slider.css('left', pos*100+'%')
+            var pos = (event.clientX-navbar.offset().left)/navbar.width(),
+                normedPos = Math.min(1, Math.max(0, pos));
+            slider.css('left', normedPos*100+'%');
         }
-        var $window = $(window);
+        var $window = $(window),
+            transition = PrefixFree.prefixCSS('transition:none').split(':');
+        slider.css(transition[0], transition[1]);
         $window.on('mousemove', moveHandler);
-        $window.one('mouseup', function(event) {
-            selectSilde(getNearestSlide(parseInt(slider.css('left'))/navbar.width()));
+        $window.one('mouseup', function() {
+            selectSlide(getNearestSlide(parseInt(slider.css('left'))/navbar.width()));
+            slider.css(transition[0], "");
             $window.off('mousemove', moveHandler)
-        })
+        });
 
     });
 
